78856a87fbb968ba4a755475b237f4932f342a2e
Lei Hsu <ge159951@gmail.com>
2022-02-19 09:55:33 +0800

    chore : add import sorter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
     'airbnb',
+    'prettier',
+    "next",
+    "next/core-web-vitals"
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,10 +23,19 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'react-hooks',
+    'jsx-a11y',
+    'import'
   ],
   rules: {
+    'ident': off,
     "react/react-in-jsx-scope": off,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'no-nested-ternary': off,
+    'react-hooks/rules-of-hooks': error,
+    '@typescript-eslint/no-empty-function': ["warn"],
+    "allowElseIf": false,
+    "no-var": true
   },
   settings: {
     'import/resolver': {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import { Provider } from "react-redux";
-import type { AppProps } from "next/app";
+import { Provider } from 'react-redux';
 
-import store from "@Redux/store";
+import store from '@Redux/store';
 
-import "../styles/globals.css";
+import '../styles/globals.css';
+
+import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
